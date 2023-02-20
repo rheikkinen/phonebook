5ad38dc0d58221008f509e8ee20c9fe2d5cba422
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-02-20 16:38:38 +0200

    Delete phonebook entries from the database
    Exercise 3.15

--- index.js
+++ index.js
@@ -59,10 +59,11 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => console.log(error))
 })
 
 const PORT = process.env.PORT || 3001
