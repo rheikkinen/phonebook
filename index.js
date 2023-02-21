eba17ad2306dc2f56075f5a94dd35d30d1bc0ec3
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-02-21 12:25:09 +0200

    Getting person by id and info route updated
    Exercise 3.18

--- index.js
+++ index.js
@@ -13,27 +13,29 @@ app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] -- :response-time ms -- :body'))
 app.use(cors())
 
-app.get('/info', (request, response) => {
-    response.send(
-        `<p>${new Date()}</p>
-        <p>The phonebook has info for ${persons.length} people.</p>`
-    )
+app.get('/info', (request, response, next) => {
+    Person.countDocuments({}).then(personCount => {
+        response.send(
+            `<p>${new Date()}</p>
+             <p>The phonebook has info for ${personCount} people.</p>`
+        )
+    }).catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons => {
         response.json(persons)
-    })
+    }).catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -44,9 +46,6 @@ app.post('/api/persons', (request, response, next) => {
             error: 'Name and number are required'
         })
     }
-
-    // TODO: Check if name already exists
-    // ...
 
     const person = new Person({
         name: body.name,
@@ -80,7 +79,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 const errorHandler = (error, _request, response, next) => {
-    console.error(error.message)
+    console.error(`${error.name}: ${error.message}`)
 
     if (error.name == 'CastError') {
         return response.status(400).send({
